90a8e1492df735e1d49410ab4e7c0f9a25a526d3
Michael Zanga <zangamj@appstate.edu>
2016-11-23 16:24:44 -0500

    Finished swipes with gesture recognizers

--- Swipes/Swipes/ViewController.swift
+++ Swipes/Swipes/ViewController.swift
@@ -10,32 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var label: UILabel!
-    private var startPoint: CGPoint!
-    
-    private static let minLength = Float(25)
-    private static let maxVariance = Float(5)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {startPoint = touch.location(in: self.view)}
+        
+        for i in 1...5 {
+            let vertical = UISwipeGestureRecognizer(target: self, action: #selector(reportVerticalSwipe(recognizer:)))
+            vertical.direction = [.up, .down]
+            vertical.numberOfTouchesRequired = i
+            view.addGestureRecognizer(vertical)
+        
+            let horizontal = UISwipeGestureRecognizer(target: self, action: #selector(reportHorizontalSwipe(recognizer:)))
+            horizontal.direction = [.left, .right]
+            horizontal.numberOfTouchesRequired = i
+            view.addGestureRecognizer(horizontal)
+        }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            let current = touch.location(in: self.view)
-            let deltaX = fabsf(Float(current.x - startPoint.x))
-            let deltaY = fabsf(Float(current.y - startPoint.y))
-            
-            if deltaX >= ViewController.minLength && deltaY <= ViewController.maxVariance {
-                label.text = "Horizontal swipe detected"
-                DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {self.label.text = ""})
-            } else if deltaY >= ViewController.minLength && deltaX <= ViewController.maxVariance {
-                label.text = "Vertical swipe detected"
-                DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {self.label.text = ""})
-            }
+    func reportHorizontalSwipe(recognizer: UIGestureRecognizer) {
+        label.text = "\(descriptionForTouchCount(recognizer.numberOfTouches))-finger horizontal swipe detected"
+        DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {self.label.text = ""})
+    }
+    
+    func reportVerticalSwipe(recognizer: UIGestureRecognizer) {
+        label.text = "\(descriptionForTouchCount(recognizer.numberOfTouches))-finger vertical swipe detected"
+        DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {self.label.text = ""})
+    }
+    
+    func descriptionForTouchCount(_ touchCount: Int) -> String {
+        switch touchCount {
+            case 1: return "Single"
+            case 2: return "Double"
+            case 3: return "Triple"
+            case 4: return "Quadruple"
+            case 5: return "Quintuple"
+            default: return ""
         }
     }
 }
