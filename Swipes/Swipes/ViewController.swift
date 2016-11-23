5245fe9af48a21eebf390e80550c7260722b0175
Michael Zanga <zangamj@appstate.edu>
2016-11-23 16:03:43 -0500

    Finished swipes without gesture recognizer

--- Swipes/Swipes/ViewController.swift
+++ Swipes/Swipes/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
+    private var startPoint: CGPoint!
+    
+    private static let minLength = Float(25)
+    private static let maxVariance = Float(5)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {startPoint = touch.location(in: self.view)}
     }
-
-
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let current = touch.location(in: self.view)
+            let deltaX = fabsf(Float(current.x - startPoint.x))
+            let deltaY = fabsf(Float(current.y - startPoint.y))
+            
+            if deltaX >= ViewController.minLength && deltaY <= ViewController.maxVariance {
+                label.text = "Horizontal swipe detected"
+                DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {self.label.text = ""})
+            } else if deltaY >= ViewController.minLength && deltaX <= ViewController.maxVariance {
+                label.text = "Vertical swipe detected"
+                DispatchQueue.main.asyncAfter(deadline: .now()+2, execute: {self.label.text = ""})
+            }
+        }
+    }
 }
 
